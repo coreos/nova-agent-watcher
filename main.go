a529d7ca7cd86f51a7454fa0b691cf28f4daaad4
Alex Polvi <alex@polvi.net>
2014-03-12 17:02:06 -0700

    add systemd integration

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"code.google.com/p/go.exp/fsnotify"
 	"flag"
+	"fmt"
+	"github.com/coreos/coreos-cloudinit/cloudinit"
+	"github.com/coreos/go-systemd/dbus"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 )
+
+var fileHandlers = map[string]func(string) (*cloudinit.CloudConfig, error){
+	"/etc/conf.d/net": handleNet,
+}
 
 func main() {
 	var watch_dir = flag.String("watch-dir", ".", "Path to watch")
@@ -18,32 +28,19 @@ func main() {
 
 	done := make(chan bool)
 
-	fileHandlers := map[string]func(string){
-		"/etc/conf.d/net": handleNet,
-	}
-	log.Println(fileHandlers)
-
 	// Process events
 	go func() {
 		for {
 			select {
 			case ev := <-watcher.Event:
-				// the ReadFile causes an atrib event
-				if !(ev.IsCreate() || (ev.IsModify() && !ev.IsAttrib())) {
+				log.Println("got event", ev)
+				if !ev.IsCreate() {
 					continue
 				}
-				file_name, err := filepath.Rel(*watch_dir, ev.Name)
+				err := runEvent(ev.Name, watch_dir)
 				if err != nil {
-					log.Println("error getting relative path for:", ev.Name)
-					continue
+					log.Println("error handling event:", err)
 				}
-				func_key := filepath.Join("/", file_name)
-				if err != nil {
-					log.Println("error getting joining path for:", ev.Name)
-					continue
-				}
-				log.Println("handling", ev)
-				fileHandlers[func_key](ev.Name)
 			case err := <-watcher.Error:
 				log.Println("error:", err)
 				done <- true
@@ -56,7 +53,11 @@ func main() {
 		dir_path := filepath.Dir(full_path)
 		err = watcher.Watch(dir_path)
 		if err != nil {
-			log.Println("warn: not watching because directory does not exist:", dir_path)
+			log.Println("warn: error setting up watcher (dir doesn't exist?):", err)
+		}
+		err = runEvent(full_path, watch_dir)
+		if err != nil {
+			log.Println("warn: initalizing event failed:", err)
 		}
 	}
 
@@ -64,12 +65,89 @@ func main() {
 	watcher.Close()
 }
 
-func handleNet(file_path string) {
-	cmd := exec.Command("./scripts/gentoo-to-networkd", "eth0", file_path)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Println("error: not good output", err)
+func runEvent(full_path string, watch_dir *string) error {
+	if _, err := os.Stat(full_path); err != nil {
+		return err
 	}
-	log.Printf("Command finished with stdout: %v", string(out))
-	log.Println("handling:", file_path)
+	file_name, err := filepath.Rel(*watch_dir, full_path)
+	if err != nil {
+		log.Println("error getting relative path for:", full_path)
+		return err
+	}
+	func_key := filepath.Join("/", file_name)
+	if err != nil {
+		log.Println("error getting joining path for:", full_path)
+		return err
+	}
+	config, err := fileHandlers[func_key](full_path)
+	if err != nil {
+		log.Println("error in handler", err)
+		return err
+	}
+	err = runConfig(config)
+	return err
+}
+
+func runConfig(config *cloudinit.CloudConfig) error {
+	f, err := ioutil.TempFile("", "rackspace-cloudinit-")
+	if err != nil {
+		return err
+	}
+	log.Println("writing to:", f.Name())
+	_, err = f.WriteString(config.String())
+	if err != nil {
+		return err
+	}
+	// systemd-run coreos-cloudinit --file f.Name()
+	props := []dbus.Property{
+		dbus.PropDescription("Unit generated and executed by coreos-cloudinit on behalf of user"),
+		dbus.PropExecStart([]string{"/usr/bin/coreos-cloudinit", "--from-file", f.Name()}, false),
+	}
+
+	tmp_file := filepath.Base(f.Name())
+	name := fmt.Sprintf("%s.service", tmp_file)
+	log.Printf("Creating transient systemd unit '%s'", name)
+
+	conn, err := dbus.New()
+	if err != nil {
+		return err
+	}
+	_, err = conn.StartTransientUnit(name, "replace", props...)
+	return err
+}
+
+func handleNet(file_name string) (*cloudinit.CloudConfig, error) {
+	contents, err := ioutil.ReadFile(file_name)
+	if err != nil {
+		log.Println("error: could not read file", err)
+		return nil, err
+	}
+	network_str := string(contents)
+
+	re := regexp.MustCompile("eth[\\d]+")
+	eths := re.FindAllString(network_str, -1)
+
+	config := cloudinit.CloudConfig{}
+
+	configured_eths := map[string]bool{}
+	for _, eth := range eths {
+		// hack to prevent multiple regex matches from creating multiple files
+		if configured_eths[eth] {
+			continue
+		}
+		cmd := exec.Command("./scripts/gentoo-to-networkd", eth, file_name)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Println("error: not good output", err)
+			return nil, err
+		}
+		unit := fmt.Sprintf("50-%s.network", eth)
+		u := cloudinit.Unit{
+			Name:    unit,
+			Content: string(out),
+		}
+		config.Coreos.Units = append(config.Coreos.Units, u)
+		configured_eths[eth] = true
+	}
+	return &config, nil
 }
